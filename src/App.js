c5cbbaf5aebc3b72a6989dd2a7160f38f9d23dd1
Xen <bitesyouzxc@gmail.com>
2025-02-17 23:37:51 +0800

    button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
   return (
     <div>
       <h1>Hello, It's my first React App!</h1>
-       <h2>Happy Coding!!</h2>
+      <h2>Happy Coding!!</h2>
+
+      <input 
+        type="file" 
+        accept="image/*, video/*" 
+        onChange={handleFileChange} 
+      />
+      {selectedFile && (
+        <div>
+          <h3>Selected File:</h3>
+          <p>{selectedFile.name}</p>
+        </div>
+      )}
     </div>
   );
 };
